dd8cc72baa73be3eb7c1bfb18efdfa4f3caa912b
李二狗 <chanli1@creditease.cn>
2017-07-20 10:47:13 +0800

    [update] 更新宏定义命名

--- DMMacroDefinition/DMMacroDefinition.h
+++ DMMacroDefinition/DMMacroDefinition.h
@@ -16,16 +16,27 @@
 #define NSLog(...) {}
 #endif
 
+#define __AppFrame                         [__MainScreen applicationFrame]  //{{0, 20}, {320, 460}}
+//屏幕尺寸
+#define __MainScreen                       [UIScreen mainScreen]            //{{0, 0}, {320, 480}}
 //屏幕宽度
-#define kScreenWidth  [[UIScreen mainScreen] bounds].size.width
+#define __Screen_W                    (__MainScreen.bounds.size.width)
 //屏幕高度
-#define kScreenHeight [[UIScreen mainScreen] bounds].size.height
+#define __Screen_H                    (__MainScreen.bounds.size.height)
 
+//不包括状态栏的导航高度
+#define __Nav_H                       44
+//状态栏高度
+#define __StatusBar_H                 20
+//导航栏，iOS7 以前是44. 不再考虑
+#define __StatusNav_H                 (__StatusBar_H + __Nav_H)
+//tabbar height
+#define __Tabbar_H                    49
 //1像素的高度，多用于cell的底部分割线
-#define kFloatSortaPixel   1.0/[UIScreen mainScreen].scale
+#define __OnePixel (1.0 / __MainScreen.scale)
 
 //获取图片资源
-#define kDMGetImage(imageName) [UIImage imageNamed:[NSString stringWithFormat:@"%@",imageName]]
+#define __DMGetImage(imageName) [UIImage imageNamed:[NSString stringWithFormat:@"%@",imageName]]
 
 //弱引用，使用weakSelf调用
 #define __DMWeakSelf(type)  __weak typeof(type) weak##type = type;
@@ -33,37 +44,37 @@
 
 //判断当前的iPhone设备/系统版本
 //判断是否为iPhone
-#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define __ISIPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 
 //判断是否为iPad
-#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define __ISIPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 
 //判断是否为ipod
-#define IS_IPOD ([[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
+#define __ISIPOD ([[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
 
 //是否是 iPhone4 尺寸的手机
-#define IS_IPHONE4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) : NO)
+#define __ISIPHONE4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) : NO)
 
 //是否是 iPhone5 尺寸的手机
-#define IS_IPHONE5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
+#define __ISIPHONE5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
 
 //是否是 iPhone6 尺寸的手机
-#define IS_IPHONE6  ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size)) : NO)
+#define __ISIPHONE6  ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size)) : NO)
 
 //是否是 iPhone6P 尺寸的手机
-#define IS_IPHONE6P ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(1125, 2001), [[UIScreen mainScreen] currentMode].size) || CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size)) : NO)
+#define __ISIPHONE6P ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(1125, 2001), [[UIScreen mainScreen] currentMode].size) || CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size)) : NO)
 
 //获取系统版本
 //获取当前系统版本
-#define IOS_SYSTEM_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
-#define IOS_SYSTEM_STRING [[UIDevice currentDevice] systemVersion]
+#define __IOS_SYSTEM_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
+#define __IOS_SYSTEM_STRING [[UIDevice currentDevice] systemVersion]
 
 //判断 iOS 8 或更高的系统版本
-#define IS_iOS8_OR_LATER (([[[UIDevice currentDevice] systemVersion] floatValue] >=8.0)? (YES):(NO))
+#define __IS_iOS8_OR_LATER (([[[UIDevice currentDevice] systemVersion] floatValue] >=8.0)? (YES):(NO))
 //判断 iOS 9 或更高的系统版本
-#define IS_iOS9_OR_LATER (([[[UIDevice currentDevice] systemVersion] floatValue] >=9.0)? (YES):(NO))
+#define __IS_iOS9_OR_LATER (([[[UIDevice currentDevice] systemVersion] floatValue] >=9.0)? (YES):(NO))
 //判断 iOS 10 或更高的系统版本
-#define IS_iOS10_OR_LATER (([[[UIDevice currentDevice] systemVersion] floatValue] >=10.0)? (YES):(NO))
+#define __IS_iOS10_OR_LATER (([[[UIDevice currentDevice] systemVersion] floatValue] >=10.0)? (YES):(NO))
 
 //网络状态判断
 #define __IsWifiConnected     \
